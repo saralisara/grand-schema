72879645c76afb0e94fcd71fd557e2d49284c7da
Christoffer Nordin <nordin.christoffer@gmail.com>
2012-05-02 15:12:32 +0200

    Initial project

---
+++ Grand-Schema/Grand-Schema/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Grand-Schema
+//
+//  Created by Student vid Yrkeshögskola C3L on 5/2/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
